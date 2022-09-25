e5a14596f533cbd563e89df1d72b6d58e10f93de
Vadim Zmushko <zmushko.vad@yandex.ru>
2022-09-25 17:20:27 +0300

    add new file and some codes for homework_5

---
+++ Homework_5/index.js
@@ -0,0 +1,49 @@
+console.log(Math.random(0));
+console.log('string'[2]);
+let age = '18 лет';
+let firstName = 'Васe';
+
+console.log(str.concat(`${age} + ${firstName}`));
+console.log(age.concat(" на данный монет" + ' Васе'));
+
+let result = 'string'+ true;
+console.log(result);
+console.log( typeof 23 + "Str");
+
+console.log( 23 + "1");
+console.log( 23 + false);
+
+console.log(1 * '3');  
+console.log('3' * false); 
+console.log(1 * true); 
+
+console.log(1 / '3');  
+console.log('3' / false); 
+console.log(1 / true); 
+
+console.log(Boolean('0'));  
+console.log(Boolean(undefined)); 
+console.log(Boolean(null)); 
+
+console.log((0));  
+console.log(Number(undefined)); 
+console.log(Number(null)); 
+ 
+let empty
+console.log(empty);
+
+let number = Number("Любая строка");
+let num = 23;
+let bool = true;
+let str = 'Строка'
+let res = null
+let anyText = 'Строка которая будет подсчитана';
+console.log(typeof number); 
+console.log(typeof toString(num));
+console.log(typeof +bool);
+console.log(typeof Boolean(str));
+console.log('6'/'2');
+console.log(+res);
+
+
+
